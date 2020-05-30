b81de318d4245fb66549387242b55720812d2418
Luciano Ezequiel Frick <s1084207@studenti.univpm.it>
2020-05-30 19:14:58 +0200

    Inserito Addpage

--- NoQueue/NoQueue/Pages/AddPage.xaml.cs
+++ NoQueue/NoQueue/Pages/AddPage.xaml.cs
@@ -10,9 +10,9 @@ using Xamarin.Forms.Xaml;
 namespace NoQueue.Pages
 {
     [XamlCompilation(XamlCompilationOptions.Compile)]
-    public partial class Page1 : ContentPage
+    public partial class AddPage : ContentPage
     {
-        public Page1()
+        public AddPage()
         {
             InitializeComponent();
         }
